09303f6ae68f0ff50f7922b1af444571fe2dfb34
Saurabh Shankariya <sshankariya@gmail.com>
2022-06-02 12:34:38 +0530

    Update index.tsx

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello There ! I'm</h5>
         <h1 className="animate-charcter">सौरभ शंकरिया</h1>
-        <h5 className="text-light">Software Engineer</h5>
+        <h5 className="text-light">Principal Engineer</h5>
 
         <CTA />
 
