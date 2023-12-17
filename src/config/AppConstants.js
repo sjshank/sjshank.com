c0c79e9ba5cb228c117077b68955413e61435d7c
Saurabh Shankariya <sshankariya@gmail.com>
2023-12-17 16:54:20 +0530

    Update AppConstants.js

--- src/config/AppConstants.js
+++ src/config/AppConstants.js
@@ -1,6 +1,6 @@
 export const DEVELOPER_CONSTANT = ["Frontend-Developer", "Blockchain Developer", "UX Designer"];
 
-export const ABOUT_ME = `I am a seasoned software developer who has 7 years of experience in Frontend, UX designing and Blockchain engineering. I am passionate about building insightful and creative software that improves the lives of those around me.
+export const ABOUT_ME = `I am a seasoned software developer who has 10+ years of experience in Frontend, UX designing and Blockchain engineering. I am passionate about building insightful and creative software that improves the lives of those around me.
 In my free time, I enjoy making short movies, sound editing and experimenting web technologies.`;
 
 export const TOAST_OPTIONS = {
